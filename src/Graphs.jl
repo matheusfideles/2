6e6f6baf67e3a9951419e9df414c59d80e5f603c
matheusfideles <67912259+matheusfideles@users.noreply.github.com>
2024-07-01 14:37:05 -0300

    Update Graphs.jl

--- src/Graphs.jl
+++ src/Graphs.jl
@@ -9,26 +9,12 @@ function scatter_error(x,y,metodo)
     savefig("images/Erro Relativo - "*metodo*".png")
 end
 
-function scatter_error_log(x,y,metodo)
-    scatter(x,log.(y),label=metodo)
-    xlabel!(L"$n$")
-    ylabel!(L"\log \left(\frac{||x-x^*||}{||x^*||} \right)")
-    savefig("images/Erro Relativo log - "*metodo*".png")
-end
-
 #Gráficos de dispersão para resíduo relativo
 function scatter_residue(x,y,metodo)
     scatter(x,y,label=metodo)
     xlabel!(L"$n$")
     ylabel!(L"$\frac{||Ax-b||}{||b||}$")
     savefig("images/Residuos - "*metodo*".png")
-end
-
-function scatter_residue_log(x,y,metodo)
-    scatter(x,log.(y),label=metodo)
-    xlabel!(L"$n$")
-    ylabel!(L"\log \left(\frac{||Ax-b||}{||b||} \right)")
-    savefig("images/Residuos log - "*metodo*".png")
 end
 
 #Gráficos de dispersão para tempo
@@ -40,7 +26,7 @@ function scatter_time(x,y,metodo)
 end
 
 #Gráficos de reta/dispersão para um dado método com o desempenho de tempo do algoritmo, residuo e erro relativo para um conjunto de dimensões n
-function plot_lines(dim)
+function plot_scatter(dim)
     #Eixo x do gráfico são as dimensões
     n=size(dim,1)
 
@@ -48,10 +34,13 @@ function plot_lines(dim)
     resultPLU, resultJac=runTest(dim)
 
     #Plotando e salvando os gráficos dos erros
-    scatter_error_log(dim,resultJac[:,1],"Gauss-Jacobi")
-    scatter_residue_log(dim,resultJac[:,2],"Gauss-Jacobi")
+    scatter_error(dim,resultJac[:,1],"Gauss-Jacobi")
+    scatter_residue(dim,resultJac[:,2],"Gauss-Jacobi")
     scatter_time(dim,resultJac[:,3],"Gauss-Jacobi")
     scatter_error(dim,resultPLU[:,1],"LU com Piv. Parcial")
     scatter_residue(dim,resultPLU[:,2],"LU com Piv. Parcial")
     scatter_time(dim,resultPLU[:,3],"LU com Piv. Parcial")
 end
+
+dim=[i for i=1000:1000:10000]
+#plot_scatter(dim)
